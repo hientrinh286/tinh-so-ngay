e91358f83d2a84435d4d69e8664ac3f28d99210b
hientrinh286 <hientrinh286@gmail.com>
2021-05-27 20:36:14 +0700

    update

--- my.js
+++ my.js
@@ -1,12 +1,17 @@
 function date (){
     let month = +prompt('Nhập tháng bạn muốn hiển thị ngày: ');
-    if (month == 1 || month == 3 || month == 5 || month == 7 || month ==8 || month == 10 || month ==12) {
-        document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 31';
-    }
-    else if (month ==2){
-        document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 28';
+    if (month>=1 && month<=12){
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month ==8 || month == 10 || month ==12) {
+            document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 31';
+        }
+        else if (month ==2){
+            document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 28';
+        }
+        else {
+            document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 30';
+        }
     }
     else {
-        document.getElementById('result').innerHTML='Ngày trong tháng ' + month + ' là: 30';
+            alert('Bạn vui lòng nhập lại tháng');
     }
-    }
+}
